2172de6ea1a06185a426beae61f882bb2ec0cf8a
juniormargalho <juniormargalho@gmail.com>
2020-05-15 20:05:43 -0300

    mascaraValorTelefone

--- app/src/main/java/com/juniormargalho/olx/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/juniormargalho/olx/activity/CadastrarAnuncioActivity.java
@@ -3,14 +3,42 @@ package com.juniormargalho.olx.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.juniormargalho.olx.R;
+import com.santalu.maskedittext.MaskEditText;
+
+import java.util.Locale;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
+    private MaskEditText campoTelefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        inicializarComponentes();
+    }
+
+    public void salvarAnuncio(View view){
+        String valor = campoValor.getText().toString();
+        Toast.makeText(this, "Valor:" + valor, Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void inicializarComponentes(){
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+        campoTelefone = findViewById(R.id.editTelefone);
+
+        Locale locale = new Locale("pt", "BR");
+        campoValor.setLocale(locale);
     }
 }
