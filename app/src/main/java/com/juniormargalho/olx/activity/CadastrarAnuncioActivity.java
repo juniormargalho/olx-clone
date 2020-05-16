68a9619ee2d133e573007f3ec093d312594b6a29
juniormargalho <juniormargalho@gmail.com>
2020-05-16 14:58:51 -0300

    permissoes

--- app/src/main/java/com/juniormargalho/olx/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/juniormargalho/olx/activity/CadastrarAnuncioActivity.java
@@ -1,7 +1,12 @@
 package com.juniormargalho.olx.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -9,6 +14,7 @@ import android.widget.Toast;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.juniormargalho.olx.R;
+import com.juniormargalho.olx.helper.Permissoes;
 import com.santalu.maskedittext.MaskEditText;
 
 import java.util.Locale;
@@ -17,11 +23,16 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
     private EditText campoTitulo, campoDescricao;
     private CurrencyEditText campoValor;
     private MaskEditText campoTelefone;
+    private String[] permissoes = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        Permissoes.validarPermissoes(permissoes, this, 1);
 
         inicializarComponentes();
     }
@@ -40,5 +51,30 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
         Locale locale = new Locale("pt", "BR");
         campoValor.setLocale(locale);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int permissaoResultado : grantResults){
+            if(permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar o App é necessário aceitar todas as permissões!");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
